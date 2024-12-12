6ac9598cc37bfe73441922ef31ea7b1a7f9f0aea
Oleg <scor1@ukr.net>
2024-12-12 13:55:11 +0100

    make build

---
+++ index.173215fd.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var r=Array.from(e.children),o=r[1].cloneNode(!0);e.insertBefore(o,r[r.length-1])});
+//# sourceMappingURL=index.173215fd.js.map
